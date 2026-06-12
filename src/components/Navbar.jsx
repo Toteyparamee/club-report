4efe6760f3407943c3ce097cdab8fe9b5a7068ba
Totey <Paramee.pho@gmail.com>
2026-06-12 16:42:16 +0700

    refactor: remove unused navbar links from Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,6 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <span className="navbar-brand">ระบบรายงานการสอนชุมนุม</span>
-      <div className="navbar-links">
-        <NavLink to="/report/new" className={({ isActive }) => isActive ? 'active' : ''}>
-          กรอกรายงาน
-        </NavLink>
-        <NavLink to="/admin" className={({ isActive }) => isActive ? 'active' : ''}>
-          ระบบหลังบ้าน
-        </NavLink>
-      </div>
     </nav>
   );
 }
